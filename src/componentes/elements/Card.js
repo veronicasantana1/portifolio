8b0be00fbabf5daa7e2a56a055ecd07fd91bd594
Verônica Santana <137583641+veronicasantana1@users.noreply.github.com>
2024-02-01 21:22:15 -0300

    versao-2

--- src/componentes/elements/Card.js
+++ src/componentes/elements/Card.js
@@ -1,17 +1,33 @@
 import styles from './Card.module.css'
 import ButtonB from './ButtonB'
+import {useState} from 'react'
+
 function Card({img, title, tech, description,repo, site}) {
+
+    const [info, setInfo] = useState(false)
+
+    function InfoOn(){
+        setInfo(true)
+    }
+
+    function InfoOff(){
+        setInfo(false)
+    }
+
     return(
-        <div className={styles.card}>
-            <a href={site}>
+        <div onMouseLeave={InfoOff} className={styles.card}>
+            <a onMouseEnter={InfoOn} href={site}>
             <img src={img} alt='ERROR'></img>
             </a>
-        <section>
-            <h3>{title}</h3>
-            <p><strong>Tecnologia: {tech}</strong></p>
-            <p>{description}</p>
-            <ButtonB text='Acesse o meu repositório' link={repo}></ButtonB>
-        </section>
+
+        {info === true && (
+            <section>
+                <h3>{title}</h3>
+                <p><strong>Tecnologia: {tech}</strong></p>
+                <p>{description}</p>
+                <ButtonB text='Acesse o meu repositório' link={repo}></ButtonB>
+            </section>
+        )}
         </div>
     )
 }
--- src/componentes/sections/Presentation.js
+++ src/componentes/sections/Presentation.js
@@ -1,12 +1,45 @@
 import styles from './Presentation.module.css'
 import ButtonA from '../elements/ButtonA'
+import {useEffect, useState} from 'react'
 
 
 function Presentation (){
+    const [text, setText] = useState('')
+    const toRotate = ['Verônica Santana', 'Desenvolvedora front-end', 'Cinéfila']
+    const [loop, setLoop] = useState(0)
+    const [isDeleting, setIsDeleting] = useState (false)
+    const period = 100;
+    const [delta, setDelta] = useState(100)
+
+useEffect(() => {
+    let ticker = setInterval(()=> {
+
+        toType()
+    }, delta)
+    return()=>{clearInterval(ticker)}
+    
+},[text])
+
+const toType = () =>{
+    let i = loop % toRotate.length;
+    let fullText = toRotate[i]
+    let updatedText = isDeleting ? fullText.substring(0,text.length-1) : fullText.substring(0,text.length+1)
+
+    setText(updatedText);
+
+        if (!isDeleting && updatedText == fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        }else if (isDeleting && updatedText === ''){
+        setIsDeleting(false)
+        setDelta(period)
+        setLoop(loop+1);
+}
+}
     return(
        <div className={styles.presentation} id="Presentation">
        <h4><strong>Bem-vindo ao meu Portifólio</strong></h4>
-       <h1>Olá, Eu sou a Verônica Santana!</h1>
+       <h1>Olá, Eu sou {text}</h1>
        <p>Sou um apaixonado por tecnologia e soluções inovadoras. <br/>
         Como Product Manager, eu tenho o compromisso de resolver problemas  <br/> complexos e trazer resultados excepcionais para os negócios.  <br/> Meus projetos já geraram milhões de reais em receita anual  <br/> estou sempre em busca de novos desafios para superar.</p>
        <ButtonA text='Conecte-se comigo!' link='https://github.com/veronicasantana1'></ButtonA>
